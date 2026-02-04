b8acf23a7a00963e42c813281ca49be14341111d
Quantum5hadow <sandeepteja.gst@gmail.com>
2026-02-04 10:20:00 +0530

    Day 127 - 3640 Trionic Array II

---
+++ Daily Leetcode/2026-02-04_D127_3640_Trionic Array II.kt
@@ -0,0 +1,49 @@
+/*
+LeetCode ID  : PixelPrince
+Profile      : https://leetcode.com/u/PixelPrince/
+Submission   : https://leetcode.com/problems/trionic-array-ii/solutions/7583446/trionic-array-ii-easy-by-pixelprince-wo03
+Problem      : 3640. Trionic Array II
+Day          : 127
+Date         : 2026-02-04
+*/
+
+class Solution {
+    fun maxSumTrionic(nums: IntArray): Long {
+        val n = nums.size
+        var res = -1e16.toLong()
+        var i = 1
+
+        while (i < n - 2) {
+            var a = i
+            var b = i
+            var net = nums[a].toLong()
+
+            while (b + 1 < n && nums[b + 1] < nums[b]) {
+                net += nums[++b].toLong()
+            }
+            if (b == a) { i++; continue }
+
+            val c = b
+            var left = 0L
+            var right = 0L
+            var lx = Long.MIN_VALUE
+            var rx = Long.MIN_VALUE
+
+            while (a - 1 >= 0 && nums[a - 1] < nums[a]) {
+                left += nums[--a].toLong()
+                lx = maxOf(lx, left)
+            }
+            if (a == i) { i++; continue }
+
+            while (b + 1 < n && nums[b + 1] > nums[b]) {
+                right += nums[++b].toLong()
+                rx = maxOf(rx, right)
+            }
+            if (b == c) { i++; continue }
+
+            res = maxOf(res, lx + rx + net)
+            i = b
+        }
+        return res
+    }
+}

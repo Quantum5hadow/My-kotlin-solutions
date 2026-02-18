57eb1578fa76f5c6719499cb316145d3eec3885b
Quantum5hadow <sandeepteja.gst@gmail.com>
2026-02-18 10:20:00 +0530

    Day 141 - 693 Binary Number with Alternating Bits

---
+++ Daily Leetcode/2026-02-18_D141_693_Binary Number with Alternating Bits.kt
@@ -0,0 +1,14 @@
+/*
+LeetCode ID  : PixelPrince
+Profile      : https://leetcode.com/u/PixelPrince/
+Submission   : https://leetcode.com/problems/binary-number-with-alternating-bits/solutions/7588625/binary-number-with-alternating-bits-ll-e-ffxj
+Problem      : 693. Binary Number with Alternating Bits
+Day          : 141
+Date         : 2026-02-18
+*/
+
+class Solution {
+    fun hasAlternatingBits(n: Int) =
+        (n and (n shr 1)) == 0 && 
+        (n and (n shr 2)) == (n shr 2)
+}

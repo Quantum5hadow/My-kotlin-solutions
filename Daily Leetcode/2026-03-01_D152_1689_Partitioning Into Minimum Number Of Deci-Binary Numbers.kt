646156cadf26fe34accbc9f4360d53b5349bec70
Quantum5hadow <sandeepteja.gst@gmail.com>
2026-03-01 10:20:00 +0530

    Day 152 - 1689 Partitioning Into Minimum Number Of Deci-Binary Numbers

---
+++ Daily Leetcode/2026-03-01_D152_1689_Partitioning Into Minimum Number Of Deci-Binary Numbers.kt
@@ -0,0 +1,12 @@
+/*
+LeetCode ID  : PixelPrince
+Profile      : https://leetcode.com/u/PixelPrince/
+Submission   : https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/solutions/7616917/kotlin-easy-by-pixelprince-swoo
+Problem      : 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers
+Day          : 152
+Date         : 2026-03-01
+*/
+
+class Solution{
+    fun minPartitions(s:String)=s.maxOf{it-'0'}
+}

37d54772175d5e7df81d14667cf929551803781a
Quantum5hadow <sandeepteja.gst@gmail.com>
2026-03-06 10:20:00 +0530

    Day 157 - 1784 Check if Binary String Has at Most One Segment of Ones

---
+++ Daily Leetcode/2026-03-06_D157_1784_Check if Binary String Has at Most One Segment of Ones.kt
@@ -0,0 +1,12 @@
+/*
+LeetCode ID  : PixelPrince
+Profile      : https://leetcode.com/u/PixelPrince/
+Submission   : https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/solutions/7629579/kotlin-very-easy-by-pixelprince-ok7w
+Problem      : 1784. Check if Binary String Has at Most One Segment of Ones
+Day          : 157
+Date         : 2026-03-06
+*/
+
+class Solution{
+    fun checkOnesSegment(s:String)=!("01" in s)
+}

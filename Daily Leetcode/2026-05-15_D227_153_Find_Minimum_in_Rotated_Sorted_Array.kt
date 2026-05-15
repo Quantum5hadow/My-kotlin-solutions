7057ef84e9dd156d84fe4b6dcc8cc34d03ecdfd4
Quantum5hadow <sandeepteja.gst@gmail.com>
2026-05-15 10:20:00 +0530

    Day 227 - 153 Find Minimum in Rotated Sorted Array

---
+++ Daily Leetcode/2026-05-15_D227_153_Find_Minimum_in_Rotated_Sorted_Array.kt
@@ -0,0 +1,12 @@
+/*
+LeetCode ID  : PixelPrince
+Profile      : https://leetcode.com/u/PixelPrince/
+Submission   : https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/solutions/8262836/kotlin-easy-by-pixelprince-5ipp
+Problem      : 153. Find Minimum in Rotated Sorted Array
+Day          : 227
+Date         : 2026-05-15
+*/
+
+class Solution{
+    fun findMin(a:IntArray)=a.minOrNull()!!
+} 

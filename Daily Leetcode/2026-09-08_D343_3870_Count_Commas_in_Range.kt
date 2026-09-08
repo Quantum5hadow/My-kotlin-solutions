3eba8f9a0d2f7b325725e8f14fd6b9e9dfb2b0c0
Quantum5hadow <sandeepteja.gst@gmail.com>
2026-09-08 10:20:00 +0530

    Day 343 - 3870 Count Commas in Range

---
+++ Daily Leetcode/2026-09-08_D343_3870_Count_Commas_in_Range.kt
@@ -0,0 +1,12 @@
+/*
+LeetCode ID  : PixelPrince
+Profile      : https://leetcode.com/u/PixelPrince/
+Submission   : https://leetcode.com/problems/count-commas-in-range/solutions/8509551/kotlin-easy-by-pixelprince-zaes
+Problem      : 3870. Count Commas in Range
+Day          : 343
+Date         : 2026-09-08
+*/
+
+class Solution{
+    fun countCommas(n:Int)=if(n<1000)0 else n-999
+}

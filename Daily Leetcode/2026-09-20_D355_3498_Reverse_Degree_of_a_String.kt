046ef75798b5b5bc793222d66e1e99e02e61084a
Quantum5hadow <sandeepteja.gst@gmail.com>
2026-09-20 10:20:00 +0530

    Day 355 - 3498 Reverse Degree of a String

---
+++ Daily Leetcode/2026-09-20_D355_3498_Reverse_Degree_of_a_String.kt
@@ -0,0 +1,12 @@
+/*
+LeetCode ID  : PixelPrince
+Profile      : https://leetcode.com/u/PixelPrince/
+Submission   : https://leetcode.com/problems/reverse-degree-of-a-string/solutions/8532115/kotlin-easy-by-pixelprince-7o0v
+Problem      : 3498. Reverse Degree of a String
+Day          : 355
+Date         : 2026-09-20
+*/
+
+class Solution{
+    fun reverseDegree(s:String)=s.withIndex().sumOf{(i,c)->(i+1)*(123-c.code)}
+}
